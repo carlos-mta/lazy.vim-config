fddcf83434814c20ee281be9237f82c2051c9039
Carlos Molero <carlos@novascript.io>
2024-04-05 23:46:13 +0000

    update: replace djlint with djhtml

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.djlint,
+        null_ls.builtins.formatting.djhtml,
         null_ls.builtins.formatting.prettier,
       },
     })
